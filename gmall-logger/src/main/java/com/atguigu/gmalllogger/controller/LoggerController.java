c94e6acd74f2ac7416851a5ca78453d8b78dea6a
harry <harry@atguigu.com>
2020-12-01 11:21:16 +0800

    day02(上午)

--- gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
@@ -1,5 +1,13 @@
 package com.atguigu.gmalllogger.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.constants.GmallConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 //@RestController = @Controller+@ResponseBody
 @RestController
+@Slf4j
 public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
 
     @RequestMapping("test1")
     // @ResponseBody  //表示返回值不是页面
@@ -29,11 +41,24 @@ public class LoggerController {
     @RequestMapping("log")
     // @ResponseBody  //表示返回值不是页面
     public String getLogger(@RequestParam("logString") String logString) {
-        System.out.println(logString);
+//        System.out.println(logString);
+
+        //0.添加时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts", System.currentTimeMillis());
 
         //1.将数据落盘
+        log.info(jsonObject.toString());
 
         //2.将数据写入Kafka
+        if ("startup".equals(jsonObject.getString("type"))) {
+            //启动日志,将数据写入启动日志主题
+            kafkaTemplate.send(GmallConstant.GMALL_STARTUP_TOPIC, jsonObject.toString());
+
+        } else {
+            //事件日志,将数据写入事件日志主题
+            kafkaTemplate.send(GmallConstant.GMALL_EVENT_TOPIC, jsonObject.toString());
+        }
 
         return "success";
     }
